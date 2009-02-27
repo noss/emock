50eb9c8f79a5e50ed483e063d4283c5c5023015e
Christian Sunesson <chsu79@gmail.com>
2009-02-27 22:32:05 +0100

    Test

---
+++ test/emock_test.erl
@@ -0,0 +1,16 @@
+-module(emock_test).
+
+-compile(export_all).
+
+code_under_test(Server, X) ->
+	%% horribly complex code here,
+	Reply = gen_server:call(Server, X),
+	%% more horribly complex code here
+	Reply.
+
+echo_server(call, X) ->
+	{reply, X}.
+
+test_code() ->
+	42 = code_under_test(emock:gen_server(fun echo_server/2), 42).
+
